73882ef010c7f0623ee6c641d5d305fc34de3146
Dragon-Five <dyddh4842@naver.com>
2025-03-18 15:10:48 +0900

    login_components 완성

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,21 @@ export default defineConfig({
         index: 'index.html',
         //components
         //main
-        header: 'src/components/main_header/header.html',
-        nav: 'src/components/main_nav/main_nav.html',
-        //mypage
-        mypage: 'src/components/mypage/mypage_user_profile.html',
-        //login
-        login_header: 'src/components/login/login_header.html',
-        login_ment: 'src/components/login/login_ment.html',
+        // header: 'src/components/main_header/header.html',
+        // nav: 'src/components/main_nav/main_nav.html',
+        // //mypage
+        // mypage: 'src/components/mypage/mypage_user_profile.html',
+        // //login
+        // login_header: 'src/components/login/login_header.html',
+        // login_ment1: 'src/components/login/login_ment1.html',
+        // login_ment2: 'src/components/login/login_ment2.html',
+        // login_email: 'src/components/login/login_email.html',
+        // login_checkbox: 'src/components/login/login_checkbox',
+        // login_button: 'src/components/login/login_button',
+        // login_id: 'src/components/login/login_id',
+        // login_account: 'src/components/login/login_account',
+        // login_google: 'src/components/login/login_google',
+        // login_qr: 'src/components/login/login_qr',
       },
     },
   },
